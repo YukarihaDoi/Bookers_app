d96de36af2ea32bb36ac5856c6eea59228d4c25d
EC2 Default User <ec2-user@ip-172-31-45-7.ap-northeast-1.compute.internal>
2022-06-11 02:38:55 +0000

    [Add] 一覧画面(新規投稿不可)

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
   
+  get '/top' =>'homes#top'
   get 'books/new' 
+  
   post '/books'=> 'books#create' 
-  get 'books/index'
+  get '/books' => 'books#index'
+  
   get 'books/show'
   get 'books/edit'
-  get '/top' =>'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+# 　resources :books
+# 　Railsのルーティングは、「上からの記載順に」マッチ
 end
+# ルートを新しく作成する必要がある
+# https://railsguides.jp/routing.html参照
