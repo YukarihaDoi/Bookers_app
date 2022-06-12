bdc6528465f8e8945083514a881de2ba9f1fbd51
EC2 Default User <ec2-user@ip-172-31-45-7.ap-northeast-1.compute.internal>
2022-06-12 22:52:39 +0000

    [ERROR]新規投稿ができない

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,34 +1,50 @@
 class BooksController < ApplicationController
-  def new
-    # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
-    @book = Book.new
-  end
 
   def index
     @books = Book.all
   end
+
+  # def new
+  #   @book = Book.new
+  # end
 
   def create
     book = Book.new(book_params)
     book.save
     redirect_to show_path(book.id)
   end
-  
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
   def show
     @book = Book.find(params[:id])
   end
 
+  # 編集(edit&update)
   def edit
+    @book = Book.find(params[:id])
   end
-  
-  private
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to show_path(book.id)
+  end
+
   # ストロングパラメータ
+  private
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
-end
 
 # フォーム画面を表示する(get)
 # フォーム(form_with)にデータを入力し、送信する(post)
 # 送信内容をデータベースへ保存する(save)
 # 保存した後に別のページに移動する(rediret_to)
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,22 @@
 Rails.application.routes.draw do
-  
-  get '/top' =>'homes#top'
-  get 'books/new' 
-  
-  post '/books'=> 'books#create' 
-  get '/books' => 'books#index',as:'index'
-  
-  get 'books/:id' => 'books#show',as:'show'
-  get 'books/edit'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-# 　resources :books
-# 　Railsのルーティングは、「上からの記載順に」マッチ
-end
-# ルートを新しく作成する必要がある
+  get '/top' =>'homes#top'
+  # OK
+
+  get 'a/books' => 'books#index',as: 'index'
+  post '/a'=> 'books#create',as: 'create'
+
+  delete 'a/books/:id' => 'books#destroy', as: 'destroy_book'
+  get 'a/books/:id' => 'books#show',as: 'show'
+
+  get 'a/books/:id/edit' => 'books#edit' , as: 'edit_book'
+  patch 'a/books/:id' => 'books#update' , as: 'update_book'
+
+# resources :books
+# Railsのルーティングは、「上からの記載順に」マッチ
+ # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+
 # https://railsguides.jp/routing.html参照
+end
